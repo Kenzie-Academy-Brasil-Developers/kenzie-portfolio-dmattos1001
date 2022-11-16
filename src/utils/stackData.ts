3a6a5f35b9d6bacc9d08bd337f1cc7d3409c1403
DanielMattos <dmattos1001@hotmail.com>
2022-11-16 19:19:39 -0300

    minor changes

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -2,10 +2,9 @@ import imgHTML from "../public/static/img/stack/html.svg";
 import imgCSS from "../public/static/img/stack/css.svg";
 import imgJS from "../public/static/img/stack/js.svg";
 import imgNode from "../public/static/img/stack/node.svg";
-import imgStyled from "../public/static/img/stack/styled.svg";
 import imgReact from "../public/static/img/stack/react.svg";
-import { SiTypescript } from "react-icons/si";
-import { FaDocker } from "react-icons/fa";
+import { SiDjango, SiTypescript } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
 import { FaFigma } from "react-icons/fa";
 
 export const stackData = [
@@ -26,14 +25,12 @@ export const stackData = [
     img: imgNode,
   },
   {
-    title: "Styled Components",
-    img: imgStyled,
-  },
-  {
     title: "React",
     img: imgReact,
   },
 
   { title: "TypeScript", img: SiTypescript },
   { title: "Figma", img: FaFigma },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
 ];
